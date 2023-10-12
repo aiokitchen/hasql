a954f7e5bf11bb713fcf7c6fea036877cea925dc
Pavel Mosein <p.mosein@edadeal.ru>
2023-10-12 12:06:11 +0600

    Add tests for metric for all drivers

--- tests/test_trouble.py
+++ tests/test_trouble.py
@@ -4,37 +4,45 @@ from unittest import mock
 import pytest
 from async_timeout import timeout
 
-from hasql.aiopg import PoolManager
+from tests.conftest import setup_aiopg, setup_aiopgsa, setup_asyncpg, setup_asyncsqlalchemy, setup_psycopg3
 
 
-@pytest.fixture
-async def pool_manager(pg_dsn):
-    pg_pool = PoolManager(
-        dsn=pg_dsn,
-        fallback_master=True,
-        pool_factory_kwargs={"minsize": 10, "maxsize": 10},
-    )
-    try:
-        await pg_pool.ready()
-        yield pg_pool
-    finally:
-        await pg_pool.close()
-
-
-async def test_unavailable_db(localhost, db_server_port):
+@pytest.mark.parametrize(
+    "pool_manager_factory",
+    [
+        (setup_aiopg),
+        (setup_aiopgsa),
+        (setup_asyncpg),
+        (setup_asyncsqlalchemy),
+        (setup_psycopg3),
+    ]
+)
+async def test_unavailable_db(pool_manager_factory, localhost, db_server_port):
     async with timeout(1):
         pg_dsn = f"postgres://pg:pg@{localhost}:{db_server_port}/pg"
-        pg_pool = PoolManager(dsn=pg_dsn, fallback_master=True)
-        await pg_pool.close()
+        async with pool_manager_factory(pg_dsn) as pool_manager:
+            pass
 
 
-async def test_catch_cancelled_error(pool_manager):
-    assert pool_manager.available_pool_count > 0
-    with mock.patch(
-        "hasql.aiopg.PoolManager._is_master",
-        side_effect=asyncio.CancelledError(),
-    ):
+@pytest.mark.parametrize(
+    "pool_manager_factory,name",
+    [
+        (setup_aiopg, "aiopg"),
+        (setup_aiopgsa, "aiopg_sa"),
+        (setup_asyncpg, "asyncpg"),
+        (setup_asyncsqlalchemy, "asyncsqlalchemy"),
+        (setup_psycopg3, "psycopg3"),
+    ]
+)
+async def test_catch_cancelled_error(pool_manager_factory, pg_dsn, name):
+    async with pool_manager_factory(pg_dsn) as pool_manager:
+        await pool_manager.ready()
+        assert pool_manager.available_pool_count > 0
+        with mock.patch(
+            f"hasql.{name}.PoolManager._is_master",
+            side_effect=asyncio.CancelledError(),
+        ):
+            await pool_manager.wait_next_pool_check()
+            assert pool_manager.available_pool_count == 0
         await pool_manager.wait_next_pool_check()
-        assert pool_manager.available_pool_count == 0
-    await pool_manager.wait_next_pool_check()
-    assert pool_manager.available_pool_count > 0
+        assert pool_manager.available_pool_count > 0
